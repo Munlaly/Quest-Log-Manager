8de6d6dfae4ccda49b77530d48b7c59606f9630a
Munlaly <nemetdbence@gmail.com>
2026-04-02 14:17:54 +0200

    main.py set up, it parses config.json

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from midterm-assignment!")
+import sys
+import json
+
+
+def load_config(path: str = "config.json"):
+    try:
+        with open(path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"CRITICAL ERROR: Configuration file '{path}' not found.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"CRITICAL ERROR: '{path}' is not valid JSON.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    config = load_config()
+
+    # set up files and dirs
+    quest_file = config.get("quest_file")
+    inventory_file = config.get("inventory_file")
+    reports_dir = config.get("repors_dir")
+    processes_dir = config.get("processes_dir")
+
+    print("--- Booting QuestLog System ---")
+    print(f"Loaded Quest File Path: {quest_file}")
+    print(f"Loaded Inventory File Path: {inventory_file}")
+    print(f"Loaded Reports Directory Path: {reports_dir}")
+    print(f"Loaded Processes Directory Path: {processes_dir}\n")
